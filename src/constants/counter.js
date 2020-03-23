062955d41dfdba10f5e265c3f2c0b784ee5c81a8
xing_ye <787747709@qq.com>
2020-03-23 18:10:25 +0800

    101010

--- src/constants/counter.js
+++
@@ -1,6 +0,0 @@
-export const ADD = 'ADD'
-export const MINUS = 'MINUS'
-export const LIST = 'LIST'
-
-export const SUCCESS_CODE = 0
-export const SUCCESS_MSG = '成功'
---
+++ src/constants/counter.ts
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line import/prefer-default-export
+export declare enum reqType {
+  ADD = 'ADD',
+  MINUS = 'MINUS',
+  LIST = 'LIST'
+}
